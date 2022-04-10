c55040232657ef83ccd3aa501335dba1076b3be5
movilidadagil <testerselenium34@gmail.com>
2022-04-10 15:48:50 +0300

    Initial Commit

---
+++ IOSSwiftUIKit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IOSSwiftUIKit
+//
+//  Created by Hasan Hüseyin Ali Gül on 10.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
